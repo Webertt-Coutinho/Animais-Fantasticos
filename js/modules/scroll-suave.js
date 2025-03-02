ccaa7f0f57633f680ef3531867d7c92762717218
Webertt Coutinho <webertt.coutinho@gmail.com>
2025-03-02 16:03:35 -0300

    Atualizações

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,18 +1,24 @@
 export default function initScrollSuave() {
-    function scrollToSection(event) {
-        event.preventDefault();
-        const href = event.currentTarget.getAttribute('href');
-        const section = document.querySelector(href);
-        const topo = section.offSetTop;
-    
-        section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
-    
-    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]')
-    linksInternos.forEach((link) => {
-        link.addEventListener('click', scrollToSection)
-    })
+  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
 }
